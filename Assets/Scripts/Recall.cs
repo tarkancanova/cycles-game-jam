ca1c1b7d7a415b001409257b37361c87f3dacec9
Mehmet Sezgin <44436956+sadstorylogan@users.noreply.github.com>
2023-07-31 19:47:15 +0300

    Recall Ability

---
+++ Assets/Scripts/Recall.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recall : MonoBehaviour
+{
+    [SerializeField] public float maxDuration;
+    [SerializeField] private float saveInterval;
+    [SerializeField] float recallSpeed;
+
+    [SerializeField] List<Vector3> positions;
+
+    private bool recalling;
+    private float saveStatsTimer;
+    private float maxStatsStored;
+
+    private void Start()
+    {
+        maxStatsStored = maxDuration / saveInterval;
+    }
+
+    private void Update()
+    {
+        PlayerRecall();
+    }
+
+    private void PlayerRecall()
+    {
+        if (!recalling)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && positions.Count > 0)
+            {
+                recalling = true;
+            }
+
+            if (saveStatsTimer > 0)
+            {
+                saveStatsTimer -= Time.deltaTime;
+            }
+            else
+            {
+                StoreStats();
+            }
+        }
+        else
+        {
+            if (positions.Count > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, positions[0], recallSpeed * Time.deltaTime);
+
+                float dist = Vector3.Distance(transform.position, positions[0]);
+                if (dist < 0.25f)
+                {
+                    SetStats();
+                }
+            }
+            else
+            {
+                recalling = false;
+            }
+        }
+    }
+
+    private void StoreStats()
+    {
+        saveStatsTimer = saveInterval;
+
+        positions.Insert(0, transform.position);
+
+        if (positions.Count > maxStatsStored)
+        {
+            positions.RemoveAt(positions.Count - 1);
+        }
+
+    }
+
+    private void SetStats()
+    {
+        transform.position = positions[0];
+
+        positions.RemoveAt(0);
+    }
+
+}
