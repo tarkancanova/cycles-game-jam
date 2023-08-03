ef150efe1a2663d1cc1b9f204476d0283bc4d86c
Tarkan Canova <tarkancanova@gmail.com>
2023-08-03 17:39:06 +0300

    Added the endless tile generation.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -107,4 +107,11 @@ public class PlayerMovement : MonoBehaviour
 
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Tile"))
+            Destroy(other.gameObject);
+    }
 }
